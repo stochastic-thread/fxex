ec41f00fd0ed5c6e6af95cdfb8aba3209bbc1610
ArthurColle <acolle@yandex.com>
2015-04-24 01:57:43 -0400

    Need extra command line argument when you want a supervisor

--- lib/fxex.ex
+++ lib/fxex.ex
@@ -1,2 +1,25 @@
 defmodule Fxex do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # Define workers and child supervisors to be supervised
+      # worker(Fxex.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Fxex.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def work do
+    def start do
+        url = "http://openexchangerates.org/api/latest.json?app_id=2bcca613e8034f2cbd416a027258d599"
+        IO.puts url
+    end
 end
